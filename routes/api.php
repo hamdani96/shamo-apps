e1f776294f6b57604721787aa0ee5eef46b09eaf
hamdani96 <hamdani.hams456@gmail.com>
2022-06-16 17:37:38 +0700

    add other API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\ProductCategoryController;
 use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\TransactionController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,8 +18,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('user', [UserController::class, 'fetch']);
+    Route::post('user', [UserController::class, 'updateProfile']);
+    Route::post('logout', [UserController::class, 'logout']);
+
+    Route::get('transaction', [TransactionController::class, 'all']);
 });
 
 Route::get('product', [ProductController::class, 'all']);
